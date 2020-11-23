d1ba0febf801a970c98f6efe876b8c3db8e5e689
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-11-23 10:15:44 -0800

    feat: start and stop elector based on Validator being synced or not

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"sync"
 	"syscall"
 	"time"
@@ -25,6 +28,7 @@ var refreshPeriod = time.Second
 var leaseDuration = 15 * time.Second
 var renewDeadline = (2 * leaseDuration) / 3
 var retryPeriod = 2 * time.Second
+var maxAgeOfLastBlock = 60 * time.Second
 
 func main() {
 	klog.InitFlags(nil)
@@ -86,12 +90,16 @@ func main() {
 			klog.Infof("Checking validator state")
 		}
 
-		synced := validator.isSynced()
-		if synced {
-			klog.Infof("Starting elector")
+		synced, err := validator.isSynced()
+
+		if err != nil {
+			klog.Warningf("Error getting validator status: %v", err)
+			elector.stop()
+		} else if synced {
+			klog.Infof("Validator is synced, starting elector")
 			elector.start(ctx)
 		} else {
-			klog.Infof("Stopping elector")
+			klog.Infof("Validator is not synced, stopping elector")
 			elector.stop()
 		}
 	}
@@ -128,13 +136,13 @@ func newValidator(rpcURL string) (*Validator, error) {
 
 			switch op {
 			case "shutdown":
-				validator.rpc("istanbul_stopValidating")
+				validator.rpc("istanbul_stopValidating", nil)
 				klog.Info("Validator shutdown")
 				return
 			case "start":
-				validator.rpc("istanbul_startValidating")
+				validator.rpc("istanbul_startValidating", nil)
 			case "stop":
-				validator.rpc("istanbul_stopValidating")
+				validator.rpc("istanbul_stopValidating", nil)
 			}
 		}
 	}()
@@ -142,26 +150,30 @@ func newValidator(rpcURL string) (*Validator, error) {
 	return &validator, nil
 }
 
-func (validator *Validator) rpc(method string) {
+func (validator *Validator) rpc(method string, params[]interface{}) (interface{}, error) {
+	if params == nil {
+		params = make([]interface{}, 0)
+	}
 	client := resty.New()
 
 	resp, err := client.R().
-		SetBody(map[string]interface{}{"jsonrpc": "2.0", "method": method, "params": []int{}, "id": 89999}).
+		SetBody(map[string]interface{}{"jsonrpc": "2.0", "method": method, "params": params, "id": 89999}).
 		Post(validator.rpcURL)
 	if err != nil {
 		klog.Warningf("HTTP %v failed: %v", method, err)
-	} else {
-		var body map[string]interface{}
-		if err := json.Unmarshal(resp.Body(), &body); err != nil {
-			klog.Warningf("failed to unmarshal response '%v': %v", resp, err)
-			return
-		}
-		if _, error := body["error"]; error {
-			klog.Warningf("RPC %v failed: %v", method, resp)
-			return
-		}
-		klog.Infof("%v succeeded: %v", method, resp)
+		return nil, err
 	}
+
+	var body map[string]interface{}
+	if err := json.Unmarshal(resp.Body(), &body); err != nil {
+		klog.Warningf("failed to unmarshal response '%v': %v", resp, err)
+		return nil, err
+	}
+	if _, error := body["error"]; error {
+		klog.Warningf("RPC %v failed: %v", method, resp)
+		return nil, errors.New("RPC failed")
+	}
+	return body["result"], nil
 }
 
 func (validator *Validator) start() {
@@ -177,9 +189,63 @@ func (validator *Validator) shutdown() {
 	validator.wg.Wait()
 }
 
-func (validator *Validator) isSynced() bool {
-	// TODO(sbw): add RPC calls and "synced" logic
-	return true
+func (validator *Validator) currentBlock() (string, error) {
+	// Read operations have no effect on the desired validator state, so execute
+	// them without serializing them via the controller loop.
+
+	result, err := validator.rpc("eth_blockNumber", nil)
+	if err != nil {
+		return "", err
+	}
+	blockNumber, ok := result.(string)
+	if !ok {
+		return "", fmt.Errorf("ethBlockNumber unexpected value: %v", result)
+	}
+	return blockNumber, nil
+}
+
+func (validator *Validator) blockTimestamp(block string) (int64, error) {
+	// Read operations have no effect on the desired validator state, so execute
+	// them without serializing them via the controller loop.
+
+	params := []interface{}{block, true}
+	result, err := validator.rpc("eth_getBlockByNumber", params)
+	if err != nil {
+		return 0, err
+	}
+	blockData, ok := result.(map[string]interface{})
+	if !ok {
+		return 0, fmt.Errorf("Unexpected result: %v", result)
+	}
+	timestampStr, ok := blockData["timestamp"].(string)
+	if !ok {
+		return 0, fmt.Errorf("Missing timestamp: %v", result)
+	}
+	timestamp, err := strconv.ParseInt(timestampStr, 0, 64)
+	if err != nil {
+		return 0, err
+	}
+	return timestamp, nil
+}
+
+func (validator *Validator) isSynced() (bool, error) {
+	block, err := validator.currentBlock()
+	if err != nil {
+		return false, err
+	}
+	timestamp, err := validator.blockTimestamp(block)
+	if err != nil {
+		return false, err
+	}
+	blockTime := time.Unix(timestamp, 0)
+	now := time.Now()
+
+	klog.Infof("Block %v is from %v", block, blockTime)
+	diff := now.Sub(blockTime)
+	if diff < maxAgeOfLastBlock {
+		return true, nil
+	}
+	return false, nil
 }
 
 // Elector controls the Kubernetes leader election.
