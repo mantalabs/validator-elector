19a61d9239c597ce54c4d08dc1b941e411bccc62
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-12-20 15:44:08 -0800

    refactor: RPC (#15)

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -29,6 +28,39 @@ var leaseDuration = 15 * time.Second
 var renewDeadline = (2 * leaseDuration) / 3
 var retryPeriod = 2 * time.Second
 var maxAgeOfLastBlock = 60 * time.Second
+
+type ErrorResponse struct {
+	Jsonrpc string
+	Id      int
+	Error   struct {
+		Code    int
+		Message string
+	}
+}
+
+type EthBlockNumber struct {
+	Jsonrpc string
+	Id      int
+	Result  string
+}
+
+type EthGetBlockByNumber struct {
+	Jsonrpc string
+	Id      int
+	Result  struct {
+		Timestamp string
+	}
+}
+
+type IstanbulStopValidating struct {
+	Jsonrpc string
+	Id      int
+}
+
+type IstanbulStartValidating struct {
+	Jsonrpc string
+	Id      int
+}
 
 func main() {
 	klog.InitFlags(nil)
@@ -110,8 +142,8 @@ func main() {
 // Validator controls a Celo Validator via JSONRPC.
 type Validator struct {
 	channel chan string
-	wg *sync.WaitGroup
-	rpcURL string
+	wg      *sync.WaitGroup
+	rpcURL  string
 }
 
 func newValidator(rpcURL string, allowPrimary bool) (*Validator, error) {
@@ -138,15 +170,15 @@ func newValidator(rpcURL string, allowPrimary bool) (*Validator, error) {
 
 			switch op {
 			case "shutdown":
-				validator.rpc("istanbul_stopValidating", nil)
+				validator.rpc("istanbul_stopValidating", nil, IstanbulStopValidating{})
 				klog.Info("Validator shutdown")
 				return
 			case "start":
 				if allowPrimary {
-					validator.rpc("istanbul_startValidating", nil)
+					validator.rpc("istanbul_startValidating", nil, IstanbulStartValidating{})
 				}
 			case "stop":
-				validator.rpc("istanbul_stopValidating", nil)
+				validator.rpc("istanbul_stopValidating", nil, IstanbulStopValidating{})
 			}
 		}
 	}()
@@ -154,30 +186,28 @@ func newValidator(rpcURL string, allowPrimary bool) (*Validator, error) {
 	return &validator, nil
 }
 
-func (validator *Validator) rpc(method string, params[]interface{}) (interface{}, error) {
-	if params == nil {
-		params = make([]interface{}, 0)
-	}
+func (validator *Validator) rpc(method string, params []interface{}, result interface{}) (interface{}, error) {
 	client := resty.New()
 
 	resp, err := client.R().
 		SetBody(map[string]interface{}{"jsonrpc": "2.0", "method": method, "params": params, "id": 89999}).
+		SetResult(result).
 		Post(validator.rpcURL)
 	if err != nil {
 		klog.Warningf("HTTP %v failed: %v", method, err)
 		return nil, err
 	}
 
-	var body map[string]interface{}
-	if err := json.Unmarshal(resp.Body(), &body); err != nil {
+	var errorResponse ErrorResponse
+	if err := json.Unmarshal(resp.Body(), &errorResponse); err != nil {
 		klog.Warningf("failed to unmarshal response '%v': %v", resp, err)
 		return nil, err
 	}
-	if _, error := body["error"]; error {
-		klog.Warningf("RPC %v failed: %v", method, resp)
-		return nil, errors.New("RPC failed")
+	if errorResponse.Error.Code != 0 {
+		return nil, fmt.Errorf("RPC error: %v", string(resp.Body()))
 	}
-	return body["result"], nil
+
+	return resp.Result(), nil
 }
 
 func (validator *Validator) start() {
@@ -197,14 +227,12 @@ func (validator *Validator) currentBlock() (string, error) {
 	// Read operations have no effect on the desired validator state, so execute
 	// them without serializing them via the controller loop.
 
-	result, err := validator.rpc("eth_blockNumber", nil)
+	response, err := validator.rpc("eth_blockNumber", nil, EthBlockNumber{})
 	if err != nil {
 		return "", err
 	}
-	blockNumber, ok := result.(string)
-	if !ok {
-		return "", fmt.Errorf("ethBlockNumber unexpected value: %v", result)
-	}
+	blockNumber := response.(*EthBlockNumber).Result
+
 	return blockNumber, nil
 }
 
@@ -213,22 +241,17 @@ func (validator *Validator) blockTimestamp(block string) (int64, error) {
 	// them without serializing them via the controller loop.
 
 	params := []interface{}{block, true}
-	result, err := validator.rpc("eth_getBlockByNumber", params)
+	response, err := validator.rpc("eth_getBlockByNumber", params, EthGetBlockByNumber{})
 	if err != nil {
 		return 0, err
 	}
-	blockData, ok := result.(map[string]interface{})
-	if !ok {
-		return 0, fmt.Errorf("Unexpected result: %v", result)
-	}
-	timestampStr, ok := blockData["timestamp"].(string)
-	if !ok {
-		return 0, fmt.Errorf("Missing timestamp: %v", result)
-	}
-	timestamp, err := strconv.ParseInt(timestampStr, 0, 64)
+	result := response.(*EthGetBlockByNumber).Result
+
+	timestamp, err := strconv.ParseInt(result.Timestamp, 0, 64)
 	if err != nil {
 		return 0, err
 	}
+
 	return timestamp, nil
 }
 
@@ -257,11 +280,11 @@ type Elector struct {
 	clientset *kubernetes.Clientset
 	validator *Validator
 
-	leaseName string
+	leaseName      string
 	leaseNamespace string
-	nodeID string
+	nodeID         string
 
-	wg *sync.WaitGroup
+	wg     *sync.WaitGroup
 	cancel context.CancelFunc
 }
 
@@ -288,7 +311,7 @@ func (elector *Elector) start(ctx context.Context) {
 		// https://pkg.go.dev/k8s.io/client-go/tools/leaderelection
 		lock := &resourcelock.LeaseLock{
 			LeaseMeta: metav1.ObjectMeta{
-				Name: elector.leaseName,
+				Name:      elector.leaseName,
 				Namespace: elector.leaseNamespace,
 			},
 			Client: elector.clientset.CoordinationV1(),
@@ -298,11 +321,11 @@ func (elector *Elector) start(ctx context.Context) {
 		}
 
 		config := leaderelection.LeaderElectionConfig{
-			Lock: lock,
+			Lock:            lock,
 			ReleaseOnCancel: true,
-			LeaseDuration: leaseDuration,
-			RenewDeadline: renewDeadline,
-			RetryPeriod: retryPeriod,
+			LeaseDuration:   leaseDuration,
+			RenewDeadline:   renewDeadline,
+			RetryPeriod:     retryPeriod,
 			Callbacks: leaderelection.LeaderCallbacks{
 				OnStartedLeading: func(ctx context.Context) {
 					elector.validator.start()
